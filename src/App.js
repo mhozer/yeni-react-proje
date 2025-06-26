ebc1c8097d565756dcb48bdd5d066b86647cc09c
mhozer <hozermiray@gmail.com>
2025-06-26 16:32:30 +0300

    giriş yazısı nermin tarafından değiştirildi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Archi's Academy Github Kursuna Hoşgeldiniz!
+        Git&Github öğreniyorum w/nermin
         </p>
         <a
           className="App-link"
